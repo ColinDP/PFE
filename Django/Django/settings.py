3427c5dca88f20c27b8cd3458312c9a38eefede4
Colin <72812770+ColinDP@users.noreply.github.com>
2020-11-30 14:48:56 +0100

    settings.py DATABASES

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'ok',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
