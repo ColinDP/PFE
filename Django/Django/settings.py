65d5db09bf2c67fc733c4da5cadc0993f0163895
SweetPieT <=>
2020-12-03 16:23:00 +0100

    commit

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -156,3 +156,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+
+SESSION_SAVE_EVERY_REQUEST=True
+
+
--- Django/app/urls.py
+++ Django/app/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     # url(r'^api/tutorials$', views.tutorial_list),
     # url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
     # url(r'^api/tutorials/published$', views.tutorial_list_published)
-    url(r'^api/login$', views.login),
+    url(r'^api/login$', views.login_request),
     url(r'^api/register$', views.register),
-    url(r'^api/get_code$', views.get_qr_code)
+    url(r'^api/get_code$', views.get_qr_code),
+    url(r'^api/logout$', views.logout_request)
+
 
 ]
