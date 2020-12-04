8db3bb54f297d11c05282b9bec1327488c3632a5
Colin <colin.deproft@student.vinci.be>
2020-12-04 17:40:23 +0100

    views2.py : handle_app_launched

--- Django/app/models.py
+++ Django/app/models.py
@@ -43,3 +43,6 @@ class Doctor(models.Model):
 class Connection(models.Model):
     user_id = models.IntegerField()
     expire_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+class Phones(models.Model):
+    phone_id = models.CharField(max_length=50, blank=False, primary_key = True)
