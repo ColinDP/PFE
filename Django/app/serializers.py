d46fdabbe7b333d12e0bd2ed4a860eb6791d9b93
Colin <colin.deproft@student.vinci.be>
2020-12-07 11:47:17 +0100

    joli serializer

--- Django/app/serializers.py
+++ Django/app/serializers.py
@@ -5,50 +5,56 @@ class ConnectionSerializer(serializers.ModelSerializer):
  
     class Meta:
         model = Connection
-        fields = ('user_id',
-                  'expire_date')
+        fields = (
+            'user_id',
+            'expire_date'
+        )
 
 class EstablishmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Establishment
-        fields = ('user_id',
-                  'name',
-                  'telephone',
-                  'street_name',
-                  'house_number',
-                  'postcode',
-                  'tva',
-                  'mail'
-                    )
+        fields = (
+            'user_id',
+            'name',
+            'telephone',
+            'street_name',
+            'house_number',
+            'postcode',
+            'tva',
+            'mail'
+        )
 
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
-        fields = ('user_id',
-                  'firstname',
-                  'lastname',
-                  'telephone',
-                  'street_name',
-                  'house_number',
-                  'postcode',
-                  'inami',
-                  'mail'
-                    )
+        fields = (
+            'user_id',
+            'firstname',
+            'lastname',
+            'telephone',
+            'street_name',
+            'house_number',
+            'postcode',
+            'inami',
+            'mail'
+        )
 
 class Qrcode_DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Qrcode_Doctor
-        fields = ('qrcode_id',
-                  'doctor',
-                  'used'         
-                    )
+        fields = (
+            'qrcode_id',
+            'doctor',
+            'used'         
+        )
 
 class Qrcode_EstablishmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Qrcode_Establishment
-        fields = ('qrcode_id',
-                  'establishment'
-                    )
+        fields = (
+            'qrcode_id',
+            'establishment'
+        )
 
 class PhonesSerializer(serializers.ModelSerializer):
     class Meta:
